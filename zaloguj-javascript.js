05b0872db52b1bc04c6b6f46d3cbeca2835bee14
Jednaczwarta <122560376+Jednaczwarta@users.noreply.github.com>
2023-01-21 17:57:57 +0100

    Add files via upload

---
+++ zaloguj-javascript.js
@@ -0,0 +1,6 @@
+function zmiana() {
+    document.getElementById("form2").style.visibility = "visible"
+    document.getElementById("form1").style.visibility = "hidden"
+    document.getElementById("form2").style.animationName = "slidein"
+    document.getElementById("form2").style.animationDuration = "1s"
+}
